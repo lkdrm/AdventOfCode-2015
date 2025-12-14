b73fb021720b993bb4e37ddc7209b0092cc9db8b
LkDrm <larik115@gmail.com>
2025-12-15 00:27:57 +0100

    End of day challenge 2

--- AdventOfCode2015/AdventOfCode2015/Program.cs
+++ AdventOfCode2015/AdventOfCode2015/Program.cs
@@ -8,9 +8,14 @@ Console.ForegroundColor = legendDefault;
 PrettyPrintExtensions.PrintWelcome();
 #endregion
 
-#region Day 1
+#region Day 1: Not Quite Lisp
 PrettyPrintExtensions.PrintResult("Day 1: Not Quite Lisp", 1, Day1.ResultPart1(ReadTaskExtensions.ReadText("Day1.txt", 1)));
 PrettyPrintExtensions.PrintResult("Day 1: Not Quite Lisp", 2, Day1.ResultPart2(ReadTaskExtensions.ReadText("Day1.txt", 1)));
+#endregion
+
+#region Day 2: I Was Told There Would Be No Math
+PrettyPrintExtensions.PrintResult("Day 2: I Was Told There Would Be No Math", 1, Day2.ResultPart1(ReadTaskExtensions.ReadTextLines("Day2.txt", 2)));
+PrettyPrintExtensions.PrintResult("Day 2: I Was Told There Would Be No Math", 2, Day2.ResultPart2(ReadTaskExtensions.ReadTextLines("Day2.txt", 2)));
 #endregion
 
 Console.ReadLine();
